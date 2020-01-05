a47755c2aad2085e9276148d3b1433626a77fbf6
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-05 23:32:47 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -57,6 +57,7 @@ class MusicLibraryController
     input = gets.chomp
     index = 1
     artist = Artist.find_by_name(input)
+    binding.pry
     if artist
       artist.songs.sort! do |song1, song2|
         song1.name <=> song2.name
