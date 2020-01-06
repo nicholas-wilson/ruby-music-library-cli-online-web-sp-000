c487dddb8ab937699bf791a5a476d68ca9dce94d
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-06 00:07:17 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,7 +18,7 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.chomp
-      MusicImporter.run_commands(input)
+      run_commands(input)
     end
   end
 
